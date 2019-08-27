acc3e1cb3575df241097a64d7a7ae54b5cb91db5
EngStu <engstu2015@gmail.com>
2019-08-27 11:20:43 +0630

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <div className="App">
         <header className="App-header">
           <Navbar />
+          <Switch>
+            <Route path='/' component={Dashboard} />
+          </Switch>
         </header>
       </div>
     </BrowserRouter>
